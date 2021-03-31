9208bce3c485135359b509cf9087939ad2aed0c8
rdogm13 <drg6525@yahoo.com>
2021-03-30 21:16:34 -0700

    TastyPie API resource registered -- successful Postman tests

--- ProductChecker/urls.py
+++ ProductChecker/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from tastypie.api import Api
+from api.resources import ProductResource
+
+product_resource = Api(api_name="v1")
+product_resource.register(ProductResource())
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(product_resource.urls))
 ]
